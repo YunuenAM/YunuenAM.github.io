0738d200346fa568a8ada9733e1a5b207aa881c9
Yunuen AM <yunameza@outlook.com>
2023-05-22 18:47:35 -0600

    ✨Feat: modal in login added

---
+++ src/js/login.js
@@ -0,0 +1,29 @@
+//Añadimos un evento al ícono de 'Ingresa' para que al dar click acceda a la página de login a través d eun modal.
+
+
+  document.getElementById("icono-ingresa").addEventListener("click", function() {
+    // Aquí puedes agregar el código para abrir el modal de compras
+    const modal = document.getElementById("modal-ingresa");
+    modal.style.display = "block";
+
+   // Obtener referencia al elemento de cierre del modal
+const closeBtn = document.querySelector(".close");
+
+// Agregar evento de click al elemento de cierre
+closeBtn.addEventListener("click", function() {
+  const modal = document.getElementById("modal-ingresa");
+  modal.style.display = "none";
+});
+
+    // Función para mostrar el modal
+function mostrarModal() {
+    modal.style.display = "block";
+  }
+  
+  // Función para ocultar el modal
+  function ocultarModal() {
+    modal.style.display = "none";
+  }
+  });
+
+
