831d8db1ff6157d49b87f54cfa188fc30a156ed9
Yunuen AM <yunameza@outlook.com>
2023-05-29 21:36:10 -0600

    ✨Feat: img at filter added

--- src/js/blog.js
+++ src/js/blog.js
@@ -8,7 +8,9 @@ const database = [
     {
       Titulo: 'Título 2',
       contenido: 'Contenido 2',
-      autor: 'Alex'
+      autor: 'Alex',
+      imagen: 'img/01-baguette.jpg'
+      
     },
     {
       Titulo: 'Título 3',
@@ -48,14 +50,30 @@ const database = [
       noResult();
     } else {
       entries.forEach(entry => {
-        const entryHTML = document.createElement('p');
-        entryHTML.textContent = `${entry.Titulo} ${entry.contenido} ${entry.autor}`;
-  
-        resultContainer.appendChild(entryHTML);
-      });
-    }
+        const entryHTML = document.createElement('div');
+        const image = document.createElement('img');
+      image.src = entry.imagen; 
+     image.style.width = '100px';
+     image.style.height = 'auto';
+      entryHTML.appendChild(image);
+
+      const title = document.createElement('h3');
+      title.textContent = entry.Titulo;
+      entryHTML.appendChild(title);
+
+      const content = document.createElement('p');
+      content.textContent = entry.contenido;
+      entryHTML.appendChild(content);
+
+      const author = document.createElement('p');
+      author.textContent = `Autor: ${entry.autor}`;
+      entryHTML.appendChild(author);
+
+      resultContainer.appendChild(entryHTML);
+    });
   }
-  
+}
+    
   // Función para borrar el contenido del contenedor de resultados
   function clearHTML() {
     resultContainer.innerHTML = '';
