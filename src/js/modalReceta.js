290e568c51e0345900449cde1c107892084677d0
Yunuen AM <yunameza@outlook.com>
2023-06-05 13:38:45 -0600

    ✨Feat: bootstrap.bundle.min removed

--- src/js/modalReceta.js
+++
@@ -1,8 +0,0 @@
-const btnVerMas = document.getElementById('btn1');
-const modal = document.getElementById('modal2');
-
-function abrirModal(){
-    modal.style.display = "block";
-}
-
-btnVerMas.addEventListener('click', abrirModal);
