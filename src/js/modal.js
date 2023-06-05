b3bc6fc3f3747b9cc8bbaed3d16d658febbe1499
Yunuen AM <yunameza@outlook.com>
2023-06-05 13:14:27 -0600

    ✨Feat:  evento DOMContentLoaded added to modal js

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,19 +2,37 @@
 
 //Añadimos un evento al ícono de 'Ingresa' para que al dar click acceda a la página de login a través de un modal.
 
-document.getElementById("icono-ingresa").addEventListener("click", function() {
-    // Aquí puedes agregar el código para abrir el modal de compras
+
+document.addEventListener("DOMContentLoaded", function() {
+  const iconoIngresa = document.getElementById("icono-ingresa");
+// Verificar si el elemento se encuentra correctamente
+ if(iconoIngresa){
+  iconoIngresa.addEventListener("click", function() {
     const modal = document.getElementById("modal-ingresa");
     modal.style.display = "block";
-
-   // Obtener referencia al elemento de cierre del modal
-const closeBtn = document.querySelector(".close");
-
-// Agregar evento de click al elemento de cierre
-closeBtn.addEventListener("click", function() {
-  const modal = document.getElementById("modal-ingresa");
-  modal.style.display = "none";
+  });
+}
 });
+
+  const closeBtn = document.querySelector(".close");
+  closeBtn.addEventListener("click", function(){
+    const modal = document.getElementById("modal-ingresa");
+    modal.style.display = "none"
+  });
+
+// document.getElementById("icono-ingresa").addEventListener("click", function() {
+//     // Aquí puedes agregar el código para abrir el modal de compras
+//     const modal = document.getElementById("modal-ingresa");
+//     modal.style.display = "block";
+
+//    // Obtener referencia al elemento de cierre del modal
+// const closeBtn = document.querySelector(".close");
+
+// // Agregar evento de click al elemento de cierre
+// closeBtn.addEventListener("click", function() {
+//   const modal = document.getElementById("modal-ingresa");
+//   modal.style.display = "none";
+// });
 
     // Función para mostrar el modal
 function mostrarModal() {
@@ -25,4 +43,3 @@ function mostrarModal() {
   function ocultarModal() {
     modal.style.display = "none";
   }
-  });
