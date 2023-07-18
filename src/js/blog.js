63f9681c633a56415b3d5b2aa6b3a3153e57cba4
Yunuen AM <yunameza@outlook.com>
2023-07-18 09:03:52 -0600

    ✨Feat:  images updated

--- src/js/blog.js
+++ src/js/blog.js
@@ -5,7 +5,7 @@ const database = [
       title: "Grandma's bread",
       content: 'Bread is one of the oldest and most appreciated foods in the world. Each culture has its own version, but there is something special about bread made by grandmothers. That homemade bread, with its unique aroma and flavor, brings with it a sense of nostalgia and warmth that transports us to our most precious memories. In this article, we will explore the magic of Grandma´s Bread and how this culinary treasure becomes the center of attention in many families.',
       author: 'Hannah',
-      imagen: '/src/img/hannah.png'
+      imagen: '../img/hannah.png'
       
     },
     {
@@ -13,26 +13,26 @@ const database = [
       title: 'Sourdough bread',
       content: "Welcome to my blog, bread lovers! Today I want to share with you a special recipe that will allow you to enjoy a delicious homemade bread using sourdough in a much shorter time than usual. Sourdough is a magical ingredient that adds flavor, texture, and durability to our breads, but it is often associated with lengthy fermentation processes. But don't worry Today I will show you how to make sourdough bread in the blink of an eye",
       author: 'John',
-      imagen: '/src/img/John.png'
+      imagen: '../img/John.png'
       
     },
     {
       title: 'Wheat',
       content: 'Did you know that wheat is one of the most important and immensely cultivated crops worldwide? With its rich history and crucial role in human nutrition, wheat has left an indelible mark on our civilization. In this blog post, we will explore some fascinating aspects of wheat in a brief overview. Wheat, scientifically known as Triticum, belongs to the grass family and is grown on almost every continent. It is considered a fundamental grain, and its versatility has made it a staple in the diet of many cultures. The wheat plant is tall, with stalks that can reach impressive heights and ears full of grains that are harvested and processed into different products.',
       author: 'Diana',
-      imagen: '/src/img/baker.png'
+      imagen: '../img/baker.png'
     },
     {
         title: 'Gluten',
         content: "Gluten has been the subject of debate and controversy in recent years. For some, it's a food villain to be avoided at all costs, while others consider it an essential part of a balanced diet. In this article, we'll explore what exactly gluten is, how it affects our bodies, and how to approach its consumption in an informed way. Gluten is a protein found in several grains, such as wheat, barley, and rye. Its main function is to provide elasticity and structure to baked goods, such as bread and cakes. However, for people with celiac disease, an autoimmune condition, gluten can trigger a damaging immune response that affects the lining of the small intestine. Additionally, some people may experience non-celiac gluten sensitivity, which causes digestive symptoms and discomfort.",
         author: 'Sara',
-        imagen: '/src/img/bakery4.png'
+        imagen: '../img/bakery4.png'
       },
       {
         title: 'Bagel',
         content: "The bagel has its roots in Eastern Europe, specifically in Jewish communities. Although its exact origin is uncertain, bagels are believed to have originated in Poland in the 17th century. These round buns became popular among Jews due to their versatility and ability to stay fresh longer. Over time, bagels spread beyond Jewish communities and became a beloved food around the world. Varieties and flavors:Today, bagels come in a wide variety of flavors and textures. From classic sesame bagels and everything to more creative options like onion, garlic, raisin or chocolate, there's a bagel for everyone. Additionally, whole grain, gluten-free, and vegan bagels can be found, making them accessible for different food preferences.",
         author: 'Julieta',
-        imagen: '/src/img/baker7.png'
+        imagen: '../img/baker7.png'
       }
   ];
 
